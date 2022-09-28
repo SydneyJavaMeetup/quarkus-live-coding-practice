c64060fd0a592dfdd544b57b24b9a7bc3b1cb5f5
Luke Thompson <endeavour9@gmail.com>
2022-09-28 11:08:36 +1000

    update to point at remote

--- load-test-k6.js
+++ load-test-k6.js
@@ -2,40 +2,19 @@
 // import http from 'k6/http';
 import ws from 'k6/ws';
 import {check, sleep} from "k6";
-// import {scenario} from 'k6/execution';
-import {Rate, Counter} from 'k6/metrics';
-
-// export function handleSummary(data) {
-//     return {
-//         "summary.html": htmlReport(data),
-//     };
-// }
 
 const isNumeric = (value) => /^\d+$/.test(value);
 
 const iterations_param = `${__ENV.ITERATIONS}`;
 const iterations = isNumeric(iterations_param) ? Number(iterations_param) : 500;
 
-const dogRate = new Rate('dogRate');
-const catRate = new Rate('catRate');
-
 export const options = {
-    vus: 5,
-    iterations: 10,
-    thresholds: {
-        dogRate: [
-            // more than 10% of errors will abort the test
-            { threshold: 'rate < 0.1', abortOnFail: true, delayAbortEval: '1m' },
-        ],
-        catRate: [
-            // more than 10% of errors will abort the test
-            { threshold: 'rate < 0.1', abortOnFail: true, delayAbortEval: '1m' },
-        ],
-    },
+    vus: 1,
+    iterations: 1
 };
 
 export default function () {
-    const url = 'ws://localhost:8080/pet-race';
+    const url = 'ws://petrace.mycodefu.com:8080/pet-race';
     const params = { };
     let messagesReceived = -2; //ignore the first two because there are two messages sent on open
     let messagesSent = 0;
@@ -54,11 +33,6 @@ export default function () {
         socket.on('message', (data) => {
             messagesReceived++;
             console.log(`Received message: ${data}. Messages sent: ${messagesSent}, Messages received: ${messagesReceived}`);
-            if (data === 'dog') {
-                dogRate.add(1);
-            } else if (data === 'cat') {
-                catRate.add(1);
-            }
             if (messagesReceived >= messagesSent && messagesSent >= iterations) {
                 socket.close();
             }
